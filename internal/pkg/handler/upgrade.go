3c8e6889ef7933297bbcae97bce9c6bf5a3dd6e8
faizanahmad055 <faizan.ahmad55@outlook.com>
2019-01-22 00:48:11 +0500

    Implement Golang CI comment
    Signed-off-by: faizanahmad055 <faizan.ahmad55@outlook.com>

--- internal/pkg/handler/upgrade.go
+++ internal/pkg/handler/upgrade.go
@@ -76,7 +76,7 @@ func PerformRollingUpgrade(client kubernetes.Interface, config util.Config, upgr
 		reloaderEnabledValue := util.ToObjectMeta(i).Annotations[constants.ReloaderAutoAnnotation]
 		if len(containers) > 0 {
 			resourceName := util.ToObjectMeta(i).Name
-			result := constants.NotUpdated
+			var result constants.Result
 			reloaderEnabled, err := strconv.ParseBool(reloaderEnabledValue)
 			if err == nil && reloaderEnabled {
 				result = updateContainers(volumes, containers, config.ResourceName, config)
