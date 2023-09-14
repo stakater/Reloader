f40824d90b5d54a75fd94e64c735a576f2695fb7
Shantanu Bansal <shantanubansal05@gmail.com>
2023-09-14 22:05:51 +0530

    Removing the fmt statement

--- internal/pkg/crypto/sha_test.go
+++ internal/pkg/crypto/sha_test.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -10,7 +9,6 @@ func TestGenerateSHA(t *testing.T) {
 	data := "www.stakater.com"
 	sha := "f9c4c51315e9ad36ec77279db875ab3f1d854b9deb77dabf7eb874427c36c2f12ab409318d3afd3e029a10913f18c0ca098a1e674fe914c5d8841f14e31542b3"
 	result := GenerateSHA(data)
-	fmt.Println(result)
 	if result != sha {
 		t.Errorf("Failed to generate SHA")
 	}
