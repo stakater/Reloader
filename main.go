a788978eae5b0b08105f4961faf8b3a0d5bcf8dc
Safwan <msafwankarim786@gmail.com>
2025-07-04 12:16:56 +0500

    enable pprof

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"os"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/stakater/Reloader/internal/pkg/app"
 )
 
 func main() {
+
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+
 	if err := app.Run(); err != nil {
 		os.Exit(1)
 	}
