0bde265fddb4917a6987845a49f942475f7ef8cf
Safwan <msafwankarim786@gmail.com>
2025-07-25 15:31:09 +0500

    enable pprof for release v1.4.5-debug

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 
 	"github.com/stakater/Reloader/internal/pkg/app"
 )
 
 func main() {
+	// Start pprof server in a goroutine
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+
 	if err := app.Run(); err != nil {
 		os.Exit(1)
 	}
