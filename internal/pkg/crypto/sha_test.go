8b64c9b9cda9ef1609e3e9dc078dd8ccc5c9bf81
faizanahmad055 <faizan.ahmad55@outlook.com>
2026-01-06 22:53:24 +0100

    Fix failing SHA test
    Signed-off-by: faizanahmad055 <faizan.ahmad55@outlook.com>

--- internal/pkg/crypto/sha_test.go
+++ internal/pkg/crypto/sha_test.go
@@ -7,7 +7,7 @@ import (
 // TestGenerateSHA generates the sha from given data and verifies whether it is correct or not
 func TestGenerateSHA(t *testing.T) {
 	data := "www.stakater.com"
-	sha := "abd4ed82fb04548388a6cf3c339fd9dc84d275df"
+	sha := "2e9aa975331b22861b4f62b7fcc69b63e001f938361fee3b4ed888adf26a10e3"
 	result := GenerateSHA(data)
 	if result != sha {
 		t.Errorf("Failed to generate SHA")
